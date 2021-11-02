8e1196b5e903ab52aaf7598b290e1d47b08f642f
Giancarlo Cavalli <gc.giancarloo@gmail.com>
2021-11-02 15:59:58 -0300

    hr-eureka-server creation

---
+++ hr-eureka-server/src/main/java/com/gcavalli/hreurekaserver/HrEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.gcavalli.hreurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrEurekaServerApplication.class, args);
+	}
+
+}
